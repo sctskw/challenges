14f7bec7586d2fbe218b4617e4de390f18a92560
Scott Skow <scott38611@comcast.net>
2014-05-14 02:07:22 -0400

    fixing tabs

--- utopiantree.py
+++ utopiantree.py
@@ -5,29 +5,29 @@ import sys
 DEBUG = False
 
 def debug(value):
-	if DEBUG:
-		print value
+    if DEBUG:
+        print value
 
 def do_height(cycle, height):
-	if not cycle & 1: # -or- cycle % 2 == 0: even number
-		debug('Height increased 1 meter')
-		return int(height + 1)
-	else:
-		debug('Height (%s) doubled in size' % height)
-		return int(height * 2)
+    if not cycle & 1: # -or- cycle % 2 == 0: even number
+        debug('Height increased 1 meter')
+        return int(height + 1)
+    else:
+        debug('Height (%s) doubled in size' % height)
+        return int(height * 2)
 
 def do_cycles(cycles):
-	height = 1
-	if cycles > 0:
-		for x in xrange(1, cycles+1):
-			height = do_height(x, int(height))
-			debug('Cycle %s: %s' % (x, height))
+    height = 1
+    if cycles > 0:
+        for x in xrange(1, cycles+1):
+            height = do_height(x, int(height))
+            debug('Cycle %s: %s' % (x, height))
 
-	return height
+    return height
 
 def run(values):
-		for value in values:
-			print do_cycles(int(value.strip()))
+        for value in values:
+            print do_cycles(int(value.strip()))
 
 if __name__ == "__main__":
-	run(sys.stdin.readlines()[1:])
+    run(sys.stdin.readlines()[1:])
--- utopiantree2.py
+++ utopiantree2.py
@@ -3,12 +3,12 @@
 import sys
 
 for line in range(input()):
-	cycles = input()
-	height = 1
-	
-	for cycle in xrange(1, cycles+1):
-		if cycle & 1:
-			height*=2
-		else:
-			height+=1
-	print height
+    cycles = input()
+    height = 1
+    
+    for cycle in xrange(1, cycles+1):
+        if cycle & 1:
+            height*=2
+        else:
+            height+=1
+    print height
