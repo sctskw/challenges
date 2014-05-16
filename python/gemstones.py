bded124cac3f10efcb34ce19f5e23f8c66e8fd25
Scott Skow <scott38611@comcast.net>
2014-05-16 02:06:37 -0400

    adding test case

--- python/gemstones.py
+++ python/gemstones.py
@@ -2,9 +2,28 @@
 
 import sys
 
+def clean(value):
+    """clean up string value"""
+    return value.strip().lower()
 
 def run():
-    pass
+    """initialize challenge"""
+
+    rocks = sys.stdin.readlines()[1:]
+    shared = None
+    value = 0
+    
+    for rock in map(clean, rocks):
+        if shared is None:
+            #need the first comparison
+            shared = set(rock)
+        shared = set(shared).intersection(set(rock))
+        
+        if len(shared) == 0:
+            #no need to check the rest
+            break
+
+    print len(list(shared or []))
 
 if __name__ == "__main__":
     run()
