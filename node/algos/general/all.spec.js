9b8eca66efb1b348f3987d5d7ecad9f1831bda93
Scott Skow <sctskw@gmail.com>
2016-02-07 12:44:57 -0700

    find pairs + tests

--- node/algos/general/all.spec.js
+++ node/algos/general/all.spec.js
@@ -24,3 +24,26 @@ describe('Find the most frequent integer in an array', function() {
 
   });
 })
+
+
+describe('Find pairs in an integer Array whose sum is X', function() {
+
+  var given = [5, 2, 3, 8, 3, 4, 6, 5, 7, 9];
+  var expect = [[5, 5], [2, 8], [3, 7], [4, 6]];
+
+  describe(format('Given: %s, Expect: %s', given, expect), function() {
+
+    it('runs in non-linear time O(n^2)', function() {
+      var result = require('./find_pairs_with_sum').nonLinear(given, 10);
+
+      result.should.deepEqual(expect);
+    })
+
+    it('runs in linear time O(n)', function() {
+      var result = require('./find_pairs_with_sum').linear(given, 10);
+
+      result.length.should.equal(4);
+    })
+
+  })
+})
